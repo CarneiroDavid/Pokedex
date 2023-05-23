0c1c4a524c6bec5ac6ca103b443d1ccc6721af64
CarneiroDavid <d.carneiro@ecole-ipssi.net>
2023-05-23 19:38:57 +0200

    reglage conflit

--- src/layouts/layouts.js
+++ src/layouts/layouts.js
@@ -4,8 +4,8 @@ const Layout = ({ children }) => {
 	return (
 		<>
 			<header className='App-header'>
-				{/* <img src={logo} className='App-logo' alt='logo' /> */}
-				{/* <Navigation></Navigation> */}
+				<img src="{logo}" className='App-logo' alt='logo' />
+				<Navigation></Navigation>
 			</header>
 
 			<main className='App-main'>{children}</main>
